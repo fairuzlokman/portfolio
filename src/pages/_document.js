affe19ef50b329791af8c8441eab22b42964b6aa
Fairuz Lokman <fairuzlokman99@gmail.com>
2023-04-06 00:39:25 +0800

    second commit

---
+++ src/pages/_document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <title>Fairuz Lokman</title>
+        <meta name="description" content="Fairuz Lokman's portfolio" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/light-bulb.png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
